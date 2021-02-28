1d3998fc79f86790c514f396ad17a9094e55f963
José Pedro Dias <jose.pedro.dias@gmail.com>
2021-02-28 16:24:23 +0000

    basic and very stupid file serialization

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 extern crate ncurses;
-extern crate serde;
-extern crate serde_json;
 
 use ncurses::*;
-use serde::{Deserialize, Serialize};
 use std::char;
+//use std::env;
 use std::fs::File;
 use std::io::prelude::*;
-use std::io::Write;
+use std::io::{BufReader, Write};
+use std::str;
+
+const FILE_FORMAT_VERSION: u8 = 1;
+
+const CH_COMMA: char = ',';
+const CH_NL: char = '\n';
 
 const UP: i32 = 65;
 const DOWN: i32 = 66;
@@ -28,7 +32,7 @@ const CLR_CURSOR: i16 = 3;
 
 const STATUS_Y: i32 = 25;
 
-const FILENAME: &str = "game.json";
+const FILENAME: &str = "game.txt";
 
 #[derive(PartialEq, Clone, Copy)]
 enum Mode {
@@ -41,7 +45,7 @@ struct Pos {
     y: usize,
 }
 
-#[derive(Clone, Debug, Serialize, Deserialize)]
+#[derive(Clone)]
 struct Cell {
     filled: bool,
     value: char,
@@ -56,7 +60,6 @@ impl Cell {
     }
 }
 
-#[derive(Debug, Serialize, Deserialize)]
 struct Matrix {
     width: usize,
     height: usize,
@@ -89,23 +92,75 @@ impl Matrix {
     }
 }
 
+fn read_str_until_char(reader: &mut BufReader<File>, ch: char) -> String {
+    let mut arr = Vec::<u8>::new();
+    reader.read_until(ch as u8, &mut arr).unwrap_or(0);
+    let s = str::from_utf8(&arr).unwrap();
+    return (&s[0..s.len() - 1]).to_string();
+}
+
+fn read_char(reader: &mut BufReader<File>) -> char {
+    let mut arr = [0u8; 1];
+    let mut ad = reader.take(1);
+    ad.read(&mut arr).unwrap_or(0);
+    return arr[0] as char;
+}
+
+fn string_to_i32(s: &String) -> i32 {
+    return s.parse::<i32>().unwrap_or(0);
+}
+
+fn string_to_bool(s: &String) -> bool {
+    return s.parse::<bool>().unwrap_or(false);
+}
+
+fn string_to_char(s: &String) -> char {
+    return s.parse::<char>().unwrap_or(' ');
+}
+
 fn load() -> Result<Matrix, std::io::Error> {
-    let mut input = File::open(FILENAME)?;
-    let mut str = String::new();
-    input.read_to_string(&mut str)?;
+    let input_file = File::open(FILENAME)?;
+    let mut buf_reader = BufReader::new(input_file);
 
-    let matrix: Matrix = serde_json::from_str(&str)?;
-    //println!("loaded: {:?}", matrix);
+    let file_format_version = read_char(&mut buf_reader) as u8;
+    read_char(&mut buf_reader);
 
-    Ok(matrix)
+    if (file_format_version - '0' as u8) != FILE_FORMAT_VERSION {
+        panic!("Unsupported format");
+    }
+
+    let w = string_to_i32(&read_str_until_char(&mut buf_reader, CH_COMMA)) as usize;
+    let h = string_to_i32(&read_str_until_char(&mut buf_reader, CH_NL)) as usize;
+
+    let mut m = Matrix::new(w, h);
+
+    for y in 0..h {
+        for x in 0..w {
+            let filled = string_to_bool(&read_str_until_char(&mut buf_reader, CH_COMMA));
+            let value = string_to_char(&read_str_until_char(&mut buf_reader, CH_NL));
+
+            let cell = m.get_cell_mut(x, y);
+            cell.filled = filled;
+            cell.value = value;
+        }
+    }
+
+    Ok(m)
 }
 
 fn save(m: &Matrix) -> Result<(), std::io::Error> {
-    let serialized = serde_json::to_string(&m)?;
-    //println!("saved: {}", serialized);
+    let mut output_file = File::create(FILENAME)?;
+    write!(output_file, "{}\n", FILE_FORMAT_VERSION);
+    write!(output_file, "{},{}\n", m.width, m.height);
 
-    let mut output = File::create(FILENAME)?;
-    write!(output, "{}", serialized)?;
+    let w = m.width;
+    let h = m.height;
+    for y in 0..h {
+        for x in 0..w {
+            let cell = m.get_cell(x, y);
+            write!(output_file, "{},{}\n", cell.filled, cell.value);
+        }
+    }
 
     Ok(())
 }
@@ -244,6 +299,7 @@ fn process_input(c: i32, m: &mut Matrix, p: &mut Pos, mode: Mode) -> (bool, Mode
 }
 
 fn main() {
+    //let args: Vec<String> = env::args().collect();
     let mut p = Pos { x: 0, y: 0 };
     let mut m = Matrix::new(11, 11);
     let mut c: i32 = 0;
